744a5839af8bb810dc0cfd9af8ccbe41777ee68d
nefelitav <nefeli.tavoulari123@gmail.com>
2025-07-13 23:43:24 +0200

    add authMiddleware

--- auth-forge-api/apps/api/routes/profile/profileRoutes.ts
+++ auth-forge-api/apps/api/routes/profile/profileRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { ProfileController } from '~/controllers/profile';
+import { authMiddleware } from '~/middleware';
 
 const profileRouter = express.Router();
 
-profileRouter.post('/request-password-reset', ProfileController.requestPasswordReset);
-profileRouter.post('/password-reset', ProfileController.resetPassword);
+profileRouter.post('/request-password-reset', authMiddleware, ProfileController.requestPasswordReset);
+profileRouter.post('/password-reset', authMiddleware, ProfileController.resetPassword);
 profileRouter.get('/', ProfileController.getUser);
 profileRouter.put('/', ProfileController.updateProfile);
 profileRouter.delete('/', ProfileController.deleteUser);
