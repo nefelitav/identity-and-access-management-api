8975427e2174b522278e431ce7775c284657a21e
nefelitav <nefeli.tavoulari123@gmail.com>
2025-09-10 22:35:28 +0200

    add authMiddleware

--- apps/api/routes/mfa/totpRoutes.ts
+++ apps/api/routes/mfa/totpRoutes.ts
@@ -1,12 +1,33 @@
 import { Router } from "express";
 import { TotpController } from "~/controllers";
 import { totpSetupLimiter, totpVerifyLimiter } from "~/utils";
+import { authMiddleware } from "~/middleware";
 
 const totpRouter = Router();
 
-totpRouter.post("/enable", totpSetupLimiter, TotpController.enable);
-totpRouter.post("/confirm", totpSetupLimiter, TotpController.confirmAndEnable);
-totpRouter.post("/verify", totpVerifyLimiter, TotpController.verify);
-totpRouter.post("/disable", totpSetupLimiter, TotpController.disable);
+totpRouter.post(
+  "/enable",
+  authMiddleware,
+  totpSetupLimiter,
+  TotpController.enable,
+);
+totpRouter.post(
+  "/confirm",
+  authMiddleware,
+  totpSetupLimiter,
+  TotpController.confirmAndEnable,
+);
+totpRouter.post(
+  "/verify",
+  authMiddleware,
+  totpVerifyLimiter,
+  TotpController.verify,
+);
+totpRouter.post(
+  "/disable",
+  authMiddleware,
+  totpSetupLimiter,
+  TotpController.disable,
+);
 
 export default totpRouter;
